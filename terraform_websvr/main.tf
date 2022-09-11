c056b1dc5279e8cadc7c00daa79c8242c0827400
tin6150 <tin_bofh_202206>
2022-09-11 10:07:09 -0700

    ec2 tf working with R 4.1.2 + 133 libs

--- terraform_websvr/main.tf
+++ terraform_websvr/main.tf
@@ -29,13 +29,21 @@ provider "aws" {
 # amazon linux use ec2user@
 # ubuntu@
 # no obvious good image for Rocky or even CentOS...
+# AMI creation based on ec2 instance (stopping req): https://docs.aws.amazon.com/toolkit-for-visual-studio/latest/user-guide/tkv-create-ami-from-instance.html
 resource "aws_instance" "webserver" {
   #ami           = "ami-09d56f8956ab235b3"  # FC eg?
   #ami           = "ami-0aab355e1bfa1e72e"  # web example, works
-  ami           = "ami-0d70546e43a941d70"  # us-west-2 oregon new ubuntu server 22.04 LTS HVM SSD root dev=ebs
+  #ami           = "ami-0d70546e43a941d70"  # us-west-2 oregon new ubuntu server 22.04 LTS HVM SSD root dev=ebs
+  #ami           = "ami-0949257a1a378f067"  # Ubuntu2204_R411_120cranLibs = ubuntu 22.04 LTS HVM SSD dev=ebs 30G + r-base 4.1.2, had 120 libs installed but then hung ## ERASE
+  ami           = " ami-00a0d9fb155b9f435"  # Ubuntu2204_R411_120cranLibs = ubuntu 22.04 LTS HVM SSD dev=ebs 30G + r-base 4.1.2, 133 libs installed 
   #ami           = "ami-0c2ab3b8efb09f272"  # AmaLin 2 HVM Kernel 5.10 SSD  ## untested
 
-  instance_type = var.instance_type
+  #instance_type = var.instance_type
+  #instance_type = "t2.micro"   # $0.020/hr  1 vCPU  0.6G RAM
+  #instance_type = "t2.small"   # $0.023/hr  1 vCPU  2G
+  #instance_type = "t2.medium"  # $0.046     2 vCPU  4G
+  #instance_type = "t3.large"   # $0.083     2 vCPU  8G
+  instance_type = "t3.xlarge"   # $0.166     4 vCPU 16G
   ## ++CHANGEME++ key_name = "EC2-keyPair-Name"  # key has to be listed by: aws ec2 describe-key-pairs
   key_name = "tin@aws2208blactam.withPass" # changing this, tf apply will destroy existing instance and recreate them.
   vpc_security_group_ids      = [aws_security_group.tf-sg.id]
@@ -46,8 +54,9 @@ resource "aws_instance" "webserver" {
     delete_on_termination = false
   }
   tags = {
-    #Name = "ExampleEC2Instance_Sn50"
-    Name = var.instance_name
+    ##Name = var.instance_name
+    Name = "TerraEC2_Sn50_libs133"
+    default = "Example EC2 Instance by Sn50 Terraform"
   }
 
   ## instance_state = stop  ## this is return status, TF can't stop or start, only create and destroy.
@@ -56,6 +65,9 @@ resource "aws_instance" "webserver" {
 
 #!/bin/bash
 
+touch /SN50_terraform.flag
+
+sudo touch /SN50_terraform.flag.sudo
 
 sudo apt-get update
 
